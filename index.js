ea8acdf0d982c47a090c5407d7e70c027ec8922c
Franzua25 <137475770+Franzua25@users.noreply.github.com>
2023-06-28 02:21:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Geri\nPium\nBot', {
+say('The\nZero\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Por Richetti`, {
+say(`Por Gabi`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
