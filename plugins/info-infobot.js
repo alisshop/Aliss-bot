d0cf37be14b5f055aa18d86d6cb06deb4284cbf1
Franzua25 <137475770+Franzua25@users.noreply.github.com>
2023-06-29 02:33:54 -0500

    Update info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -54,8 +54,8 @@ let old = performance.now()
   let speed = neww - old
 
 let info = `╭━━━━[ ${gt} ]━━━━━⬣
-┃➥ *CREADORA | CREATOR*
-┃ღ *GeriPium*
+┃➥ *CREADOR | CREATOR*
+┃ღ *Zero*
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃➥ *CONTACTO | CONTACT* 
 ┃ღ *${ig}*
