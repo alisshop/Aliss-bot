ff4d476f135794f5aac7867826c94575dfad89e9
Franzua25 <137475770+Franzua25@users.noreply.github.com>
2023-07-04 01:54:41 -0500

    Update fun-pregunta.js

--- plugins/fun-pregunta.js
+++ plugins/fun-pregunta.js
@@ -2,7 +2,7 @@ let handler = async (m, { command, text }) => m.reply(`
 *⁉️ 𝐏𝐑𝐄𝐆𝐔𝐍𝐓𝐀𝐒 ⁉️*
   
 *𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰:* ${text}
-*𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible','a mi q me importa','calla virgen','por eso no cachas','yo que soy adivinó?'].getRandom()}
+*𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible','a mi q me importa','calla virgen','por eso no cachas','yo que soy adivinó?','no preguntes webadas','anda estudia mejor','por eso te ponen los kchos'].getRandom()}
 `.trim(), null, m.mentionedJid ? {
 mentions: m.mentionedJid
 } : {})
