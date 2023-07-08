f591d884fcc5c426025c345e6db2d0f72f398056
Franzua25 <137475770+Franzua25@users.noreply.github.com>
2023-07-08 00:53:13 -0500

    Create cachudo.js

---
+++ plugins/cachudo.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, command, text }) => {
+if (!text) throw `*Ingrese el @ o el nombre de la persona que desee calcular su porcentaje de ${command.replace('how', '')}*`
+conn.reply(m.chat, `
+_*${text}* *es* *${Math.floor(Math.random() * 2000)}%* *${command.replace('how', '').toUpperCase()}.* *kchudo por idiota!! 🦌*_
+`.trim(), m, m.mentionedJid ? {
+contextInfo: {
+mentionedJid: m.mentionedJid }} : {})}
+handler.help = ['cachudo *@user*']
+handler.tags = ['game']
+handler.command = /^(puto)/i
+handler.fail = null
+module.exports = handler
