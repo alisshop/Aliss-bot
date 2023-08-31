c954af57b9f15816ab3cb0536aeca7f74b15589a
CharlieLit <136955810+CharlieLit@users.noreply.github.com>
2023-08-30 22:50:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -174,8 +174,8 @@ global.exito = eg
 
 //⊱ ━━━━━.⋅ Información | Information ⋅.━━━━ ⊰
 
-global.wm = 'ZeroBot'
-global.igfg = '@ventas_bot.thezero'
+global.wm = 'AlissBot'
+global.igfg = 'AlissBot'
 global.wait = '*⌛ _Cargando | Charging..._ ▬▭▭▭▭▭▭*'
 global.waitt = '*⌛ _Cargando | Charging..._ ▬▬▭▭▭*'
 global.waittt = '*⌛ _Cargando | Charging..._ ▬▬▬▬▭▭*'
